e1b7964bb529c8a794792ee6ba36ca49f71b7254
Ксюша Заякина <zayakina.oksana@mail.ru>
2024-07-07 21:48:43 +0200

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,7 @@ name = 'Name: Ksusha'
 print(name)
 age = 24
 print('Age:',age)
-new_age = 1
-print('New Age:',age + new_age)
+age = age + 3
+print('New Age:',age)
 is_student = True
 print('Is Student:',is_student)
