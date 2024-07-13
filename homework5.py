b83662a6691cb4dba697362e9690586e8ba8818b
Ксюша Заякина <zayakina.oksana@mail.ru>
2024-07-13 15:56:39 +0200

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,5 +1,10 @@
 immutable_var = (18, 19, 20, 'Orange', True)
 print(immutable_var)
+
+#immutable_var[4] = False
+#print(immutable_var)
+#Кортеж относится к неизменяемым типам данных и используется для хранения упорядоченной последовательности элементов, поэтому изменять его нельзя.
+
 mutable_list = ['Sun', 'Moon', 'Space']
 mutable_list[2] = 'Earth'
 print(mutable_list)
