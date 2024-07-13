59f0b01214cd5397ffee3891e8d6674236f9246a
Ксюша Заякина <zayakina.oksana@mail.ru>
2024-07-13 19:20:59 +0200

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,30 @@
+first = 18
+second = 18
+third = 18
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+first = 12
+second = 18
+third = 18
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+first = 12
+second = 16
+third = 18
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
